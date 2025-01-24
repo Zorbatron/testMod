11c4740e36c1b71d171eae5f341e51679bde289c
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-01-24 00:57:05 -0500

    blah blah

--- src/main/java/com/myname/mymodid/mixins/TestMixin.java
+++ src/main/java/com/myname/mymodid/mixins/TestMixin.java
@@ -1,24 +1,19 @@
 package com.myname.mymodid.mixins;
 
-import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = BehaviorDefaultDispenseItem.class)
+@Mixin(value = BlockTallGrass.class, remap = false)
 public class TestMixin {
 
-    @ModifyArgs(method = "dispenseStack",
-                at = @At(value = "INVOKE",
-                         target = "Lnet/minecraft/dispenser/BehaviorDefaultDispenseItem;doDispense(Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;ILnet/minecraft/util/EnumFacing;Lnet/minecraft/dispenser/IPosition;)V"))
-    private void changeItem(Args args) {
-        ItemStack original = args.get(1);
-        if (original.isItemEqual(new ItemStack(Blocks.DIAMOND_BLOCK))) {
-            args.set(1, new ItemStack(Blocks.DIRT));
-        }
+    @ModifyArg(method = "getDrops",
+               at = @At(value = "INVOKE", target = "Lnet/minecraft/util/NonNullList;add(Ljava/lang/Object;)Z"))
+    public Object getDrops(Object par1) {
+        return new ItemStack(Blocks.DIAMOND_BLOCK);
     }
 }
